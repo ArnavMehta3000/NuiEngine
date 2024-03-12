aad23e673ba0aaa3da46470a5e17e60d7422ad19
Arnav Mehta <arnavdatt@gmail.com>
2024-03-12 23:52:30 +0000

    Fix tabbing in log file comments

--- Engine/Core/Utils/Log.h
+++ Engine/Core/Utils/Log.h
@@ -4,9 +4,9 @@
 namespace Nui::Log
 {
 	/*
-    * @brief Log verbosity levels
-    * @see LogEntry
-    */
+	* @brief Log verbosity levels
+	* @see LogEntry
+	*/
 	enum class LogLevel
 	{
 		Debug = 0,   // For low level engine information
@@ -17,66 +17,66 @@ namespace Nui::Log
 		Exception,   // Similar to errors, but gives a stack trace and crashes the program
 	};
 
-    /**
+	/**
  * @class LogEntry
  * @brief Represents a log entry with its level, category, message, time, and stack trace.
  * @see LogLevel
  */
-    struct LogEntry 
-    {
-        /**
-         * @brief Constructor for LogEntry
-         * @param level The log level
-         * @param category The log category
-         * @param message The log message
-         * @param trace The stack trace
-         * @see LogLevel
-         */
-        LogEntry(LogLevel level, StringView category, StringView message, Nui::Stacktrace trace = Nui::Stacktrace::current());
+	struct LogEntry 
+	{
+		/**
+		 * @brief Constructor for LogEntry
+		 * @param level The log level
+		 * @param category The log category
+		 * @param message The log message
+		 * @param trace The stack trace
+		 * @see LogLevel
+		 */
+		LogEntry(LogLevel level, StringView category, StringView message, Nui::Stacktrace trace = Nui::Stacktrace::current());
 
-        /**
-         * @brief The log level
-         */
-        LogLevel Level;
+		/**
+		 * @brief The log level
+		 */
+		LogLevel Level;
 
-        /**
-         * @brief The log category
-         */
-        String Category;
+		/**
+		 * @brief The log category
+		 */
+		String Category;
 
-        /**
-         * @brief The log message
-         */
-        String Message;
+		/**
+		 * @brief The log message
+		 */
+		String Message;
 
-        /**
-         * @brief The timestamp of the log entry
-         */
-        chrono::system_clock::time_point Time;
+		/**
+		 * @brief The timestamp of the log entry
+		 */
+		chrono::system_clock::time_point Time;
 
-        /**
-         * @brief The stack trace associated with the log entry
-         */
-        Stacktrace Stacktrace;
-    };
+		/**
+		 * @brief The stack trace associated with the log entry
+		 */
+		Stacktrace Stacktrace;
+	};
 
-    /**
-    * @brief Logs a log entry to the VS output window.
-    * @param entry The log entry
-    * @see LogEntry
-    */
+	/**
+	* @brief Logs a log entry to the VS output window.
+	* @param entry The log entry
+	* @see LogEntry
+	*/
 	void Log(const LogEntry& entry);
 
 	/*
-    * @brief Asserts a condition and logs an error if it is not met.
-    * @param condition The condition to assert
-    * @param conditionString The string representation of the condition
-    * @param message The error message
-    * @param file The source file
-    * @param line The source line
-    * @param trace The stack trace
-    * @throws std::runtime_error if the condition is not met
-    */
+	* @brief Asserts a condition and logs an error if it is not met.
+	* @param condition The condition to assert
+	* @param conditionString The string representation of the condition
+	* @param message The error message
+	* @param file The source file
+	* @param line The source line
+	* @param trace The stack trace
+	* @throws std::runtime_error if the condition is not met
+	*/
 	void Assert(bool condition, StringView conditionString, StringView message, StringView file, I32 line, Nui::Stacktrace trace = Nui::Stacktrace::current());
 }
 
