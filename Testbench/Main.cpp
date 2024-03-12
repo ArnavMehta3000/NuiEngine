598df60c9b7ad1516149367056178d817fe5300f
Arnav Mehta <arnavdatt@gmail.com>
2024-03-12 23:52:11 +0000

    Clean test main

--- Testbench/Main.cpp
+++ Testbench/Main.cpp
@@ -3,13 +3,5 @@
 
 NUI_MAIN()
 {
-	NUI_LOG(Debug, Main, "Debug Log!");
-	NUI_LOG(Info, Main, "Info Log!");
-	NUI_LOG(Warn, Main, "Warn Log!");
-	NUI_LOG(Error, Main, "Error Log!");
-	//NUI_LOG(Fatal, Main, "Fatal Log!");
-
-	HRESULT hr = E_FAIL;
-	NUI_ASSERT(SUCCEEDED(hr), "Number is not 3");
 	return 0;
 }
