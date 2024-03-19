996994300cec99ab910ae6f52a7dd25fbb6f7c34
Arnav Mehta <arnavdatt@gmail.com>
2024-03-19 01:29:13 +0000

    Integrated input system with engine loop

--- Engine/Core/Engine/Engine.cpp
+++ Engine/Core/Engine/Engine.cpp
@@ -1,6 +1,7 @@
 #include "Core/Engine/Engine.h"
 #include "Core/Utils/Filesystem.h"
-#include "Core/Utils/Timer.h"
+#include "Core/App/Input/Input.h"
+#include <thread>
 
 namespace Nui::Engine
 {
@@ -21,13 +22,18 @@ namespace Nui::Engine
 		timer.Start();
 
 		// Open engine log file
-		Log::Internal::OpenLogFile(
-			Filesystem::GetCurrentWorkingDirectory() / "Saved" / "NuiEngine.log");
+		Log::Internal::OpenLogFile(Filesystem::GetCurrentWorkingDirectory() / "Saved" / "NuiEngine.log");
 
 		NUI_LOG(Debug, Engine, "Initializing Nui Engine...");
 
+
 		// Make application
 		s_app = Internal::MakeApp();
+
+		NUI_ASSERT(s_app.get(), "Failed to create application");
+
+		// Initialize input
+		Input::Internal::Init(s_app->GetHWND());
 
 		timer.Stop();
 		NUI_LOG(Debug, Engine, "Nui Engine initialized successfully in ", timer.GetElapsedSeconds().ToString(), " seconds");
@@ -42,10 +48,24 @@ namespace Nui::Engine
 
 		NUI_LOG(Debug, Engine, "Starting main loop...");
 
+		Timer updateLoop;
+		updateLoop.Start();
+
+		F64 now = 0.0, dt = 0.0, elapsed = 0.0;
 		while (!s_app->WantsToClose())
 		{
+			now = updateLoop.GetElapsedSeconds();
 
+			Input::Internal::PreUpdate(s_app->Focused());
+
+			// Tick engine here
+
+			Input::Internal::PostUpdate();
+
+			dt = now - elapsed;
+			elapsed = now;
 		}
+		updateLoop.Stop();
 	}
 
 	void Shutdown()
