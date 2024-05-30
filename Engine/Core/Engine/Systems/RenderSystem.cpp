9f6b86cee601f7bb6c7cf54a2808bccf11efd83c
Arnav Mehta <arnavdatt@gmail.com>
2024-05-31 00:17:27 +0100

    Present on end frame

--- Engine/Core/Engine/Systems/RenderSystem.cpp
+++ Engine/Core/Engine/Systems/RenderSystem.cpp
@@ -6,9 +6,11 @@ namespace Nui::Systems
 {
 	void RenderSystem::OnInit(ECS::Context* ctx)
 	{
-		NUI_ASSERT(Graphics::IsInitialized(), 
+		NUI_ASSERT(Graphics::IsInitialized(),
 			"RenderSystem::OnInit: Graphics not initialized. Initialize the graphics system before initializing the render system.");
-		
+
+		NUI_LOG(Debug, RenderSystem, "RenderSystem::OnInit");
+
 		NUI_SUBSCRIBE_EVENT(ctx, Events::EndFrame);
 	}
 
@@ -18,11 +20,22 @@ namespace Nui::Systems
 
 	void RenderSystem::OnShutdown(ECS::Context* ctx)
 	{
+		NUI_LOG(Debug, RenderSystem, "RenderSystem::OnShutdown");
 		NUI_UNSUBSCRIBE_EVENTS_ALL(ctx);
 	}
 
 	NUI_DEFINE_EVENT(RenderSystem, Events::EndFrame)
 	{
-		NUI_LOG(Info, RenderSystem, "RenderSystem::OnEvent - EndFrame");
+		OnEndFrame();
+	}
+
+	void RenderSystem::OnEndFrame()
+	{
+		const D3DManager& d3d = Singleton<D3DManager>::Get();
+		ID3D11DeviceContext* d3dCtx = d3d.GetImmediateContext();
+
+		d3dCtx->ClearRenderTargetView(d3d.GetBackBuffer(), DirectX::Colors::CornflowerBlue);
+
+		d3d.Present();
 	}
 }
