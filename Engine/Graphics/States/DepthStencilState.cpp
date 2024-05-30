7a7cb1c27612878069fb43b2fe9d2459873142dc
Arnav Mehta <arnavdatt@gmail.com>
2024-05-30 19:04:32 +0100

    Align equal sign

--- Engine/Graphics/States/DepthStencilState.cpp
+++ Engine/Graphics/States/DepthStencilState.cpp
@@ -5,17 +5,17 @@ namespace Nui::Graphics::States
 	constexpr D3D11_DEPTH_STENCIL_DESC GetDepthStencilDepthDisabledDesc()
 	{
 		D3D11_DEPTH_STENCIL_DESC dsDesc{};
-		dsDesc.DepthEnable = false;
-		dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ZERO;
-		dsDesc.DepthFunc = D3D11_COMPARISON_LESS_EQUAL;
-		dsDesc.StencilEnable = false;
-		dsDesc.StencilReadMask = D3D11_DEFAULT_STENCIL_READ_MASK;
-		dsDesc.StencilWriteMask = D3D11_DEFAULT_STENCIL_WRITE_MASK;
+		dsDesc.DepthEnable                  = false;
+		dsDesc.DepthWriteMask               = D3D11_DEPTH_WRITE_MASK_ZERO;
+		dsDesc.DepthFunc                    = D3D11_COMPARISON_LESS_EQUAL;
+		dsDesc.StencilEnable                = false;
+		dsDesc.StencilReadMask              = D3D11_DEFAULT_STENCIL_READ_MASK;
+		dsDesc.StencilWriteMask             = D3D11_DEFAULT_STENCIL_WRITE_MASK;
 		dsDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_KEEP;
-		dsDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
-		dsDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_REPLACE;
-		dsDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
-		dsDesc.BackFace = dsDesc.FrontFace;
+		dsDesc.FrontFace.StencilFailOp      = D3D11_STENCIL_OP_KEEP;
+		dsDesc.FrontFace.StencilPassOp      = D3D11_STENCIL_OP_REPLACE;
+		dsDesc.FrontFace.StencilFunc        = D3D11_COMPARISON_ALWAYS;
+		dsDesc.BackFace                     = dsDesc.FrontFace;
 
 		return dsDesc;
 	}
@@ -23,17 +23,17 @@ namespace Nui::Graphics::States
 	constexpr D3D11_DEPTH_STENCIL_DESC GetDepthStencilDepthEnabledDesc()
 	{
 		D3D11_DEPTH_STENCIL_DESC dsDesc{};
-		dsDesc.DepthEnable = true;
-		dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ZERO;
-		dsDesc.DepthFunc = D3D11_COMPARISON_LESS_EQUAL;
-		dsDesc.StencilEnable = false;
-		dsDesc.StencilReadMask = D3D11_DEFAULT_STENCIL_READ_MASK;
-		dsDesc.StencilWriteMask = D3D11_DEFAULT_STENCIL_WRITE_MASK;
+		dsDesc.DepthEnable                  = true;
+		dsDesc.DepthWriteMask               = D3D11_DEPTH_WRITE_MASK_ZERO;
+		dsDesc.DepthFunc                    = D3D11_COMPARISON_LESS_EQUAL;
+		dsDesc.StencilEnable                = false;
+		dsDesc.StencilReadMask              = D3D11_DEFAULT_STENCIL_READ_MASK;
+		dsDesc.StencilWriteMask             = D3D11_DEFAULT_STENCIL_WRITE_MASK;
 		dsDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_KEEP;
-		dsDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
-		dsDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_REPLACE;
-		dsDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
-		dsDesc.BackFace = dsDesc.FrontFace;
+		dsDesc.FrontFace.StencilFailOp      = D3D11_STENCIL_OP_KEEP;
+		dsDesc.FrontFace.StencilPassOp      = D3D11_STENCIL_OP_REPLACE;
+		dsDesc.FrontFace.StencilFunc        = D3D11_COMPARISON_ALWAYS;
+		dsDesc.BackFace                     = dsDesc.FrontFace;
 
 		return dsDesc;
 	}
@@ -41,17 +41,17 @@ namespace Nui::Graphics::States
 	constexpr D3D11_DEPTH_STENCIL_DESC GetDepthStencilReverseDepthEnabledDesc()
 	{
 		D3D11_DEPTH_STENCIL_DESC dsDesc{};
-		dsDesc.DepthEnable = true;
-		dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ZERO;
-		dsDesc.DepthFunc = D3D11_COMPARISON_GREATER_EQUAL;
-		dsDesc.StencilEnable = false;
-		dsDesc.StencilReadMask = D3D11_DEFAULT_STENCIL_READ_MASK;
-		dsDesc.StencilWriteMask = D3D11_DEFAULT_STENCIL_WRITE_MASK;
+		dsDesc.DepthEnable                  = true;
+		dsDesc.DepthWriteMask               = D3D11_DEPTH_WRITE_MASK_ZERO;
+		dsDesc.DepthFunc                    = D3D11_COMPARISON_GREATER_EQUAL;
+		dsDesc.StencilEnable                = false;
+		dsDesc.StencilReadMask              = D3D11_DEFAULT_STENCIL_READ_MASK;
+		dsDesc.StencilWriteMask             = D3D11_DEFAULT_STENCIL_WRITE_MASK;
 		dsDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_KEEP;
-		dsDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
-		dsDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_REPLACE;
-		dsDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
-		dsDesc.BackFace = dsDesc.FrontFace;
+		dsDesc.FrontFace.StencilFailOp      = D3D11_STENCIL_OP_KEEP;
+		dsDesc.FrontFace.StencilPassOp      = D3D11_STENCIL_OP_REPLACE;
+		dsDesc.FrontFace.StencilFunc        = D3D11_COMPARISON_ALWAYS;
+		dsDesc.BackFace                     = dsDesc.FrontFace;
 
 		return dsDesc;
 	}
@@ -59,17 +59,17 @@ namespace Nui::Graphics::States
 	constexpr D3D11_DEPTH_STENCIL_DESC GetDepthStencilDepthWriteEnabledDesc()
 	{
 		D3D11_DEPTH_STENCIL_DESC dsDesc{};
-		dsDesc.DepthEnable = true;
-		dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
-		dsDesc.DepthFunc = D3D11_COMPARISON_LESS_EQUAL;
-		dsDesc.StencilEnable = false;
-		dsDesc.StencilReadMask = D3D11_DEFAULT_STENCIL_READ_MASK;
-		dsDesc.StencilWriteMask = D3D11_DEFAULT_STENCIL_WRITE_MASK;
+		dsDesc.DepthEnable                  = true;
+		dsDesc.DepthWriteMask               = D3D11_DEPTH_WRITE_MASK_ALL;
+		dsDesc.DepthFunc                    = D3D11_COMPARISON_LESS_EQUAL;
+		dsDesc.StencilEnable                = false;
+		dsDesc.StencilReadMask              = D3D11_DEFAULT_STENCIL_READ_MASK;
+		dsDesc.StencilWriteMask             = D3D11_DEFAULT_STENCIL_WRITE_MASK;
 		dsDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_KEEP;
-		dsDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
-		dsDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_REPLACE;
-		dsDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
-		dsDesc.BackFace = dsDesc.FrontFace;
+		dsDesc.FrontFace.StencilFailOp      = D3D11_STENCIL_OP_KEEP;
+		dsDesc.FrontFace.StencilPassOp      = D3D11_STENCIL_OP_REPLACE;
+		dsDesc.FrontFace.StencilFunc        = D3D11_COMPARISON_ALWAYS;
+		dsDesc.BackFace                     = dsDesc.FrontFace;
 
 		return dsDesc;
 	}
@@ -77,17 +77,17 @@ namespace Nui::Graphics::States
 	constexpr D3D11_DEPTH_STENCIL_DESC GetDepthStencilReverseDepthWriteEnabledDesc()
 	{
 		D3D11_DEPTH_STENCIL_DESC dsDesc{};
-		dsDesc.DepthEnable = true;
-		dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
-		dsDesc.DepthFunc = D3D11_COMPARISON_GREATER_EQUAL;
-		dsDesc.StencilEnable = false;
-		dsDesc.StencilReadMask = D3D11_DEFAULT_STENCIL_READ_MASK;
-		dsDesc.StencilWriteMask = D3D11_DEFAULT_STENCIL_WRITE_MASK;
+		dsDesc.DepthEnable                  = true;
+		dsDesc.DepthWriteMask               = D3D11_DEPTH_WRITE_MASK_ALL;
+		dsDesc.DepthFunc                    = D3D11_COMPARISON_GREATER_EQUAL;
+		dsDesc.StencilEnable                = false;
+		dsDesc.StencilReadMask              = D3D11_DEFAULT_STENCIL_READ_MASK;
+		dsDesc.StencilWriteMask             = D3D11_DEFAULT_STENCIL_WRITE_MASK;
 		dsDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_KEEP;
-		dsDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
-		dsDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_REPLACE;
-		dsDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
-		dsDesc.BackFace = dsDesc.FrontFace;
+		dsDesc.FrontFace.StencilFailOp      = D3D11_STENCIL_OP_KEEP;
+		dsDesc.FrontFace.StencilPassOp      = D3D11_STENCIL_OP_REPLACE;
+		dsDesc.FrontFace.StencilFunc        = D3D11_COMPARISON_ALWAYS;
+		dsDesc.BackFace                     = dsDesc.FrontFace;
 
 		return dsDesc;
 	}
@@ -95,17 +95,17 @@ namespace Nui::Graphics::States
 	constexpr D3D11_DEPTH_STENCIL_DESC GetDepthStencilDepthStencilWriteEnabledDesc()
 	{
 		D3D11_DEPTH_STENCIL_DESC dsDesc{};
-		dsDesc.DepthEnable = true;
-		dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
-		dsDesc.DepthFunc = D3D11_COMPARISON_LESS_EQUAL;
-		dsDesc.StencilEnable = true;
-		dsDesc.StencilReadMask = D3D11_DEFAULT_STENCIL_READ_MASK;
-		dsDesc.StencilWriteMask = D3D11_DEFAULT_STENCIL_WRITE_MASK;
+		dsDesc.DepthEnable                  = true;
+		dsDesc.DepthWriteMask               = D3D11_DEPTH_WRITE_MASK_ALL;
+		dsDesc.DepthFunc                    = D3D11_COMPARISON_LESS_EQUAL;
+		dsDesc.StencilEnable                = true;
+		dsDesc.StencilReadMask              = D3D11_DEFAULT_STENCIL_READ_MASK;
+		dsDesc.StencilWriteMask             = D3D11_DEFAULT_STENCIL_WRITE_MASK;
 		dsDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_KEEP;
-		dsDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
-		dsDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_REPLACE;
-		dsDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
-		dsDesc.BackFace = dsDesc.FrontFace;
+		dsDesc.FrontFace.StencilFailOp      = D3D11_STENCIL_OP_KEEP;
+		dsDesc.FrontFace.StencilPassOp      = D3D11_STENCIL_OP_REPLACE;
+		dsDesc.FrontFace.StencilFunc        = D3D11_COMPARISON_ALWAYS;
+		dsDesc.BackFace                     = dsDesc.FrontFace;
 
 		return dsDesc;
 	}
