7a7ab5fb3dc11f235eb98a8932342f54085ef492
Arnav Mehta <arnavdatt@gmail.com>
2024-06-13 20:10:25 +0100

    Fix issues with test project

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,10 @@
 set_xmakever("2.9.2")
 
 add_rules("mode.debug", "mode.release", "mode.releasedbg")
-
 set_allowedmodes("debug", "release", "releasedbg")
+
+set_allowedplats("windows")
+set_allowedarchs("windows|x64")
 
 set_languages("clatest", "cxxlatest")
 
