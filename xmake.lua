ea561029123a952592dfeb88469b4b04de64ab0e
Arnav Mehta <arnavdatt@gmail.com>
2024-06-14 00:15:09 +0100

    Update defines

--- xmake.lua
+++ xmake.lua
@@ -1,18 +1,66 @@
+-- XMake version
 set_xmakever("2.9.2")
 
+-- Build configs
 add_rules("mode.debug", "mode.release", "mode.releasedbg")
 set_allowedmodes("debug", "release", "releasedbg")
 
+-- Platform and architecture
 set_allowedplats("windows")
 set_allowedarchs("windows|x64")
 
+-- Set C/C++ language version
 set_languages("clatest", "cxxlatest")
 
+-- Project name and version
 set_project("NuiXmake")
 set_version("0.0.1")
 
-add_defines("UNICODE")
-
+-- Generate clang compile commands
 add_rules("plugin.compile_commands.autoupdate")
 
+-- Add defines
+add_defines("UNICODE")
+
+if is_mode("debug") then
+
+    -- add macros
+    add_defines("DEBUG", "NUI_DEBUG")
+
+    -- enable debug symbols
+    set_symbols("debug")
+
+    -- disable optimization
+    set_optimize("none")
+
+end
+
+-- if the current compilation mode is release or profile?
+if is_mode("release", "releasedbg") then
+    if is_mode("release") then
+   		-- add macros
+        add_defines("RELEASE", "NUI_RELEASE", "NDEBUG")
+
+        -- mark symbols visibility as hidden
+        set_symbols("hidden")
+
+        -- strip all symbols
+        set_strip("all")
+
+        -- fomit frame pointer
+        add_cxflags("-fomit-frame-pointer")
+        add_mxflags("-fomit-frame-pointer")
+    else
+   		-- add macros
+        add_defines("RELEASE", "NUI_RELEASE")
+
+        -- enable debug symbols
+        set_symbols("debug")
+    end
+
+    -- add vectorexts
+    add_vectorexts("sse2", "sse3", "ssse3", "mmx")
+end
+
+-- Include all xmake projects
 includes("**/xmake.lua")
